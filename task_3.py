17a9eacaf1b7c02771a51cf201bb6604bb02850b
Yauheni <gek0305@gmail.com>
2022-02-16 19:07:33 +0300

    new lines in file task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,9 @@
 try:
     my_list = [int(i) for i in input("Введите числа в строку через пробел(ы): ").split()]
     my_new_list = list(filter(lambda x: x % 3 == 0, my_list))
-    print(*my_new_list)
+    if len(my_new_list) == 0:
+        print("Нет чисел, кратных 3")
+    else:
+        print("Числа кратные 3: ", *my_new_list)
 except:
     print("Вы ввели не только числа")
