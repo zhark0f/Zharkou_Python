1c331e5b472ca9ee35ae9f3dc9ad8cad7ecff94e
Yauheni <gek0305@gmail.com>
2022-02-17 13:31:53 +0300

    new version of task_3.py

--- task_3.py
+++ task_3.py
@@ -1,9 +1,13 @@
-try:
-    my_list = [int(i) for i in input("Введите числа в строку через пробел(ы): ").split()]
-    my_new_list = list(filter(lambda x: x % 3 == 0, my_list))
-    if len(my_new_list) == 0:
-        print("Нет чисел, кратных 3")
-    else:
-        print("Числа кратные 3: ", *my_new_list)
-except:
-    print("Вы ввели не только числа")
+my_line = input("Введите числа в строку через пробел(ы): ").split()
+my_list = []
+for i in my_line:
+    try:
+        temp_number = int(i)
+        if temp_number % 3 == 0:
+            my_list.append(temp_number)
+    except ValueError:
+        pass
+if len(my_list) == 0:
+    print("Нет чисел, кратных 3")
+else:
+    print("Числа кратные 3: ", *my_list)
